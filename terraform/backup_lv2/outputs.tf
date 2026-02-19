e64143c27f4b3eccb66e9a5cf1419eba3b529699
M-Benoist <benoist.marc@gmail.com>
2026-02-19 18:19:12 +0000

    Terraform & Ansible

--- terraform/backup_lv2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "web_vm_ip" {
-  value = proxmox_virtual_environment_vm.vm_web.initialization[0].ip_config[0].ipv4[0].address
-}
-
-output "db_vm_ip" {
-  value = proxmox_virtual_environment_vm.vm_db.initialization[0].ip_config[0].ipv4[0].address
-}
